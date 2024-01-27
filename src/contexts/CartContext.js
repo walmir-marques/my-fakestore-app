11b4105117b6cdb48ff24afd3139061418fe7985
Walmir Marques Filho <151250830+walmir-marques@users.noreply.github.com>
2024-01-27 18:43:17 -0300

    first commit

---
+++ src/contexts/CartContext.js
@@ -0,0 +1,100 @@
+import React, { createContext, useEffect, useState } from "react";
+
+export const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+  const [itemAmount, setItemAmount] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const total = cart.reduce((acc, currentItem) => {
+      return acc + currentItem.price * currentItem.amount;
+    }, 0);
+
+    setTotal(total);
+  });
+
+  useEffect(() => {
+    if (cart) {
+      const amount = cart.reduce((acc, currentItem) => {
+        return acc + currentItem.amount;
+      }, 0);
+      setItemAmount(amount);
+    }
+  }, [cart]);
+
+  const addToCart = (product, id) => {
+    const newItem = { ...product, amount: 1 };
+    const CartItem = cart.find((item) => {
+      return item.id === id;
+    });
+    if (CartItem) {
+      const newCart = [...cart].map((item) => {
+        if (item.id === id) {
+          return { ...item, amount: CartItem.amount + 1 };
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    } else {
+      setCart([...cart, newItem]);
+    }
+  };
+
+  const removeFromCart = (id) => {
+    const newCart = cart.filter((item) => {
+      return item.id !== id;
+    });
+    setCart(newCart);
+  };
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  const increaseAmount = (id) => {
+    const cartItem = cart.find((item) => item.id === id);
+    addToCart(cartItem, id);
+  };
+
+  const decreaseAmount = (id) => {
+    const cartItem = cart.find((item) => {
+      return item.id === id;
+    });
+
+    if (cartItem) {
+      const newCart = cart.map((item) => {
+        if (item.id === id) {
+          return { ...item, amount: cartItem.amount - 1 };
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    }
+    if (cartItem.amount < 2) {
+      removeFromCart(id);
+    }
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        addToCart,
+        removeFromCart,
+        clearCart,
+        increaseAmount,
+        decreaseAmount,
+        cart,
+        itemAmount,
+        total,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import { ProductContext } from "../contexts/ProductContext";
+import Product from "../components/Product";
+import Hero from "../components/Hero";
+
+const Home = () => {
+  //pegando os produtos do contexto
+  const { products } = useContext(ProductContext);
+  //pegando somente roupas da categoria de homens e mulheres
+  const filteredProducts = products.filter((item) => {
+    return (
+      item.category === "men's clothing" || item.category === "women's clothing"
+    );
+  });
+
+  return (
+    <div>
+      <Hero />
+      <section className="py-16">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-7 max-w-sm mx-auto md:max-w-none md:mx-0">
+            {filteredProducts.map((product) => {
+              return <Product key={product.id} product={product} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
